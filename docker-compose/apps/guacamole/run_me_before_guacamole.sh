2692be390e5f08c384bc3508c73f8ddca36ff652
jakirhosen9395 <jakirhosen9395@gmail.com>
2025-07-24 17:55:36 +0600

    updated

--- docker-compose/apps/guacamole/run_me_before_guacamole.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Creating guacamole init db"
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > conf/initdb.sql
--- docker-images/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Iniciando container..."
-echo "INICIADO!!" > /var/www/html/ini.html
-httpd -D FOREGROUND
